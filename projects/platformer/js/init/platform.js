0e6844c6d4cfa4510941977d008022398b6322dd
cleblanc123 <leblan273@stcharles.k12.la.us>
2022-08-30 13:38:55 +0000

    pushing for testing

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,13 +37,13 @@
     createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
 
     // example:
-    createPlatform(400, 235, .8, .5);
+    createPlatform(400, 220, .8, .5);
     createPlatform(75, 460, .8, .5);
 
 
     createPlatform(350, 550, .8, .5);
     createPlatform(500, 400, .8, .5)
-    createPlatform(100, 300, .8, .5);
+    createPlatform(100, 315, .8, .5);
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
