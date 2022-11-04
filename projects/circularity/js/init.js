14c5fece41cac4381af7f960f2e5aa8e57000f23
cleblanc123 <leblan273@stcharles.k12.la.us>
2022-11-04 12:58:35 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
         
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        for(var i = 1; i <= 100; i++) {
+            drawCircle();
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -40,9 +48,26 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
+            // physikz.updatePosition(circles[0]); -> Deleted because it's repetive and hardcoded
+            // physikz.updatePosition(circles[1]); -> Deleted because it's repetive and hardcoded
+            // physikz.updatePosition(circles[2]); -> Deleted because it's repetive and hardcoded
+            // physikz.updatePosition(circles[3]); -> Deleted because it's repetive and hardcoded
+            // physikz.updatePosition(circles[4]); -> Deleted because it's repetive and hardcoded
             
+            for(var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
+
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+
+            // game.checkCirclePosition(circles[0]); -> Deleted because it's repetive and hardcoded
+            // game.checkCirclePosition(circles[1]); -> Deleted because it's repetive and hardcoded
+            // game.checkCirclePosition(circles[2]); -> Deleted because it's repetive and hardcoded
+            // game.checkCirclePosition(circles[3]); -> Deleted because it's repetive and hardcoded
+            // game.checkCirclePosition(circles[4]); -> Deleted because it's repetive and hardcoded
+
+
 
             // TODO 9 : Iterate over the array
            
@@ -57,14 +82,20 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
+
+
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+            if(circle.x > canvas.width) {
+                circle.x = 0;
+            }else if(circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            if(circle.y > canvas.height) {
+                circle.y = 0;
+            }else if(circle.y < 0) {
+                circle.y = canvas.height;
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
