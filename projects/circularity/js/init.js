8534e242590de23209a7c1225be493746e2f38ad
cleblanc123 <leblan273@stcharles.k12.la.us>
2022-11-09 13:23:33 +0000

    updated circularity, challenge not working

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,30 +1,32 @@
 var init = function (window) {
     'use strict';
-    var 
+    var
         draw = window.opspark.draw,
         physikz = window.opspark.racket.physikz,
-        
+
         app = window.opspark.makeApp(),
-        canvas = app.canvas, 
+        canvas = app.canvas,
         view = app.view,
         fps = draw.fps('#000');
-        
-    
-    window.opspark.makeGame = function() {
-        
+
+
+    window.opspark.makeGame = function () {
+
         window.opspark.game = {};
         var game = window.opspark.game;
-        
+
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM SETUP ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+
         // TODO 1 : Declare and initialize our variables
         var circle;
         var circles = [];
+        // var leftAndRightEdge = circle.x + circle.radius;
+        // var bottomAndTopEdge = circle.y + circle.radius;
 
         // TODO 2 : Create a function that draws a circle 
-        
+
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
             physikz.addRandomVelocity(circle, canvas, 10, 10);
@@ -33,13 +35,13 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for(var i = 1; i <= 100; i++) {
+        for (var i = 1; i <= 100; i++) {
             drawCircle();
         }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+
         /* 
         This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
@@ -53,8 +55,8 @@ var init = function (window) {
             // physikz.updatePosition(circles[2]); -> Deleted because it's repetive and hardcoded
             // physikz.updatePosition(circles[3]); -> Deleted because it's repetive and hardcoded
             // physikz.updatePosition(circles[4]); -> Deleted because it's repetive and hardcoded
-            
-            for(var i = 0; i < circles.length; i++) {
+
+            for (var i = 0; i < circles.length; i++) {
                 physikz.updatePosition(circles[i]);
                 game.checkCirclePosition(circles[i]);
             }
@@ -70,53 +72,57 @@ var init = function (window) {
 
 
             // TODO 9 : Iterate over the array
-           
-            
+
+
         }
-    
+
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-        game.checkCirclePosition = function(circle) {
+        game.checkCirclePosition = function (circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
 
+            // var leftEdge = circle.x + circle.radius;
+            // var rightEdge = circle.x + circle.radius;
+            // var topEdge = circle.y + circle.radius;
+            // var bottomEdge = circle.y + circle.radius;
 
-            
+
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if(circle.x > canvas.width) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
-            }else if(circle.x < 0) {
+            } else if (circle.x < 0) {
                 circle.x = canvas.width;
             }
-            if(circle.y > canvas.height) {
+            if (circle.y > canvas.height) {
                 circle.y = 0;
-            }else if(circle.y < 0) {
+            } else if (circle.y < 0) {
                 circle.y = canvas.height;
             }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
-        
+
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
         /////////////////////////////////////////////////////////////
-        
+
         view.addChild(fps);
         app.addUpdateable(fps);
-        
+
         game.circle = circle;
         game.circles = circles;
         game.drawCircle = drawCircle;
         game.update = update;
-        
+
         app.addUpdateable(window.opspark.game);
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
