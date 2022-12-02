e8b85779095368684976d12d4b291f62dbc154ec
cleblanc123 <leblan273@stcharles.k12.la.us>
2022-12-02 13:49:34 +0000

    Runtime - 1. init.js line 37 and 38 are breaking the firing mechanism. 2. background.js line 88 does not move the tree backwards

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,6 +36,7 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
+            
             var backgroundFill = draw.rect(canvasWidth,canvasHeight,'black');
             background.addChild(backgroundFill);
             
@@ -49,7 +50,7 @@ var background = function (window) {
             background.addChild(forestBackGround);
 
             var moon = draw.bitmap("img/moon.png");
-            moon.x = 1400;
+            moon.x = canvasWidth - 75;
             moon.y = 40;
             moon.scaleX = .6;
             moon.scaleY = .6;
